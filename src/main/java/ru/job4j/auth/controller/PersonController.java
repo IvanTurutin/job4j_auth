e328c8e405a37d86de3796221ddca3000a5e7b81
Ivan <ivan.turutin@gmail.com>
2023-07-27 21:30:44 +0300

    5. Обработка исключений и Spring REST [#504797]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -1,13 +1,20 @@
 package ru.job4j.auth.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -17,9 +24,11 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/persons")
+@Slf4j
 public class PersonController {
     private final PersonService persons;
     private final BCryptPasswordEncoder encoder;
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/all")
     public List<Person> findAll() {
@@ -27,16 +36,16 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable int id) {
-        var person = this.persons.findById(id);
-        return new ResponseEntity<Person>(
-                person.orElse(new Person()),
-                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
-        );
+    public Person findById(@PathVariable int id) {
+        return persons.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Account is not found. Please, check requisites."
+                ));
     }
 
     @PostMapping(value = "/sign-up", consumes = {"application/json"})
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        checkPersonData(person);
         person.setPassword(encoder.encode(person.getPassword()));
         return new ResponseEntity<Person>(
                 person,
@@ -46,6 +55,8 @@ public class PersonController {
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        checkPersonData(person);
+        person.setPassword(encoder.encode(person.getPassword()));
         return this.persons.update(person) ? ResponseEntity.ok().build() : ResponseEntity.internalServerError().build();
     }
 
@@ -54,5 +65,28 @@ public class PersonController {
         Person person = new Person();
         person.setId(id);
         return this.persons.delete(person) ? ResponseEntity.ok().build() : ResponseEntity.internalServerError().build();
+    }
+
+    private void checkPersonData(Person person) {
+        if (person.getPassword() == null || person.getLogin() == null) {
+            throw new NullPointerException();
+        }
+        if (person.getPassword().length() < 6) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+        }
+        if (person.getLogin().isEmpty()) {
+            throw new IllegalArgumentException("Invalid login. Login can't by empty.");
+        }
+    }
+
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() { {
+            put("message", e.getMessage());
+            put("type", e.getClass());
+        }}));
+        log.error(e.getLocalizedMessage());
     }
 }
