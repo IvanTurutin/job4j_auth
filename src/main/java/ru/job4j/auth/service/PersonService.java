ce187b880fd66de5da396a6275f129436025f8bb
Ivan <ivan.turutin@gmail.com>
2023-08-15 21:07:57 +0300

    7. DTO [#504800]

--- src/main/java/ru/job4j/auth/service/PersonService.java
+++ src/main/java/ru/job4j/auth/service/PersonService.java
@@ -3,10 +3,15 @@ package ru.job4j.auth.service;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.repository.PersonRepository;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,15 +43,48 @@ public class PersonService {
     }
 
     public boolean update(Person person) {
-        if (findById(person.getId()).isPresent()) {
-            personRepository.save(person);
-            return true;
+        try {
+            personRepository.save(patch(person));
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Impossible invoke set method from object : " + person + ", Check set and get pairs.");
         }
         return false;
     }
 
     public boolean delete(Person person) {
         return personRepository.deleteById(person.getId());
+    }
+
+    private Person patch(Person person) throws InvocationTargetException, IllegalAccessException {
+        var currentOpt = personRepository.findById(person.getId());
+        if (currentOpt.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        var current = currentOpt.get();
+        var methods = current.getClass().getDeclaredMethods();
+        var namePerMethod = new HashMap<String, Method>();
+        for (var method: methods) {
+            var name = method.getName();
+            if (name.startsWith("get") || name.startsWith("set")) {
+                namePerMethod.put(name, method);
+            }
+        }
+        for (var name : namePerMethod.keySet()) {
+            if (name.startsWith("get")) {
+                var getMethod = namePerMethod.get(name);
+                var setMethod = namePerMethod.get(name.replace("get", "set"));
+                if (setMethod == null) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                            "Impossible invoke set method from object : " + current + ", Check set and get pairs.");
+                }
+                var newValue = getMethod.invoke(person);
+                if (newValue != null) {
+                    setMethod.invoke(current, newValue);
+                }
+            }
+        }
+        return current;
     }
 
 
